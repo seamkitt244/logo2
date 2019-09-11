9e6535d68c6715bab8e5f4ff9a81afd89fa8ee32
seamkitt244 <seamkitt244@SNS-D-130RZR>
2019-09-11 12:21:14 -0400

    FIRST COMMIT

--- LOGO2/Form1.cs
+++ LOGO2/Form1.cs
@@ -15,14 +15,14 @@ namespace LOGO2
         public Form1()
         {
             InitializeComponent();
-           
+           // this si a test comment
         }
 
         private void Form1_Shown(object sender, EventArgs e)
         {
  // Code for text creation
             Graphics g2 = this.CreateGraphics();
-            Font drawFont = new Font("Arial", 16, FontStyle.Bold);
+            Font drawFont = new Font("Arial", 30, FontStyle.Italic);
             SolidBrush drawBrush2 = new SolidBrush(Color.Red);
             // Code for line creation
             Graphics g = this.CreateGraphics();
@@ -35,18 +35,18 @@ namespace LOGO2
             g.DrawLine(drawPen, 280, 386, 545, 386);
             g.DrawLine(drawPen, 354, 171, 445, 170);
             drawPen = new Pen (Color.White, 3);
-            g.DrawLine(drawPen, 405, 150, 550, 400);
+            g.DrawLine(drawPen, 405, 150, 570, 400);
             g.DrawLine(drawPen, 240, 400, 395, 150);
-            g.DrawLine(drawPen, 325, 386, 630, 386);
+            g.DrawLine(drawPen, 279, 400, 395, 150);
+            g.DrawLine(drawPen, 325, 386, 640, 386);
             g.DrawLine(drawPen, 370, 165, 432, 165);
             // text drawing
 
             g.DrawString("PALACE", drawFont, drawBrush2, 50, 40);
-            g.TranslateTransform(200, 50);
+            g.TranslateTransform(230, 340);
+            g.RotateTransform(-60);
 
-            g.RotateTransform(60);
-
-            g.DrawString("Hello", drawFont, drawBrush, new Rectangle());
+            g.DrawString("PALACE", drawFont, drawBrush, new Rectangle());
 
             g.ResetTransform();
 
